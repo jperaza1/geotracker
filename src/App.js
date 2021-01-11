aae5249701e1a0a72178172f15d3de212d72baa1
Jose Enrique Peraza Santamaria <61806943+jperazas208@users.noreply.github.com>
2021-01-10 21:57:48 -0600

    agregando proyecto principal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [speed, setSpeed] = useState(null)
+  const [position, setPosition] = useState(null);
+  const [baterry, setBattery] = useState(null)
+
+  useEffect(() => {
+    let id = null;
+
+
+    if(navigator.geolocation){
+      id = navigator.geolocation.watchPosition(successPosition, error, { timeout: 30000 });
+      let batteryIsCharging = false;
+
+      navigator.getBattery().then(function(battery) {
+        setBattery(battery.level);
+        batteryIsCharging = battery.charging;
+
+        battery.addEventListener('chargingchange', function() {
+          batteryIsCharging = battery.charging;
+          console.log(batteryIsCharging);
+        });
+      });
+    }
+
+    return () => {
+      navigator.geolocation.clearWatch(id);
+    }
+  }, []);
+
+
+  const successPosition = (position) => {
+    setPosition(position.coords);
+    setSpeed(position.coords.speed);
+  }
+
+  const error = (err) => {
+    console.log(`Error:`+ err.code + err.message);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>GeoTracker</h1>
+
+      <p>Tu velocidad es de { speed !== null ? (`${speed.toFixed(2)} MS/S`) : '0 MS/S' }</p>
+      <p>Tus coordenadas son: {position?.latitude}, {position?.longitude}</p>
+      <p>Nivel de Baterria: {`${(baterry * 100)}%`}</p>
     </div>
   );
 }
